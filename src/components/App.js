2335a51eb623b5c2b28c8fe30cb3f32d57e5d5f9
unknown <sangnv196@gmail.com>
2019-05-05 23:30:28 +0700

    Display Channel Info in MetaPanel

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
       />
     </Grid.Column>
     <Grid.Column width={4}>
-      <MetaPanel key={currentChannel && currentChannel.id} isPrivateChannel={isPrivateChannel}/>
+      <MetaPanel key={currentChannel && currentChannel.id} isPrivateChannel={isPrivateChannel} currentChannel={currentChannel} />
     </Grid.Column>
   </Grid>
 );
